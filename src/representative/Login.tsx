bd4953f9f2d83f871447e7a553f7facf1fd3b35d
kareem-elmeteny <kareem.elmeteny@gmail.com>
2024-05-03 20:28:25 +0300

    add yum and formik to registration page.

--- src/representative/Login.tsx
+++ src/representative/Login.tsx
@@ -1,30 +1,13 @@
 import {useNavigate} from "react-router-dom";
-import {useState} from "react";
-import {representatives} from "../representative/OrganizationRepresentative";
+import {Formik} from "formik";
+import FormField from "../common/FormField.tsx";
+import {LoginRequest, loginSchema} from "./login.ts";
 
-
-function Login(){
+function Login() {
     const navigate = useNavigate();
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = async (event: { preventDefault: () => void; }) => {
-        event.preventDefault();
-        try{
-            const representative = representatives.find((representative: { username: string; password: string; }) => representative.username === username && representative.password === password);
-            if(representative) {
-                navigate('/representative');
-            }
-            else {
-                alert('Invalid username or password');
-                return;
-            }
-        }
-        catch (error){
-            alert('An error occurred');
-        }
-
-
+    const initialValues: LoginRequest = {username: '', password: ''};
+    const handleSubmit = async () => {
+        navigate('/representativehome')
     }
 
     function handleRegistration() {
@@ -32,25 +15,32 @@ function Login(){
     }
 
     return (
-        <div className="container">
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="exampleInputUsername1">Username</label>
-                    <input type="text" className="form-control" id="exampleInputUsername1" aria-describedby="usernameHelp" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <div className="form-group form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Log in</button>
-                <button type="button" className="btn btn-secondary" onClick={handleRegistration}>Don't have an account? Register</button>
-            </form>
-        </div>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={loginSchema}>
+            {
+                (formik) => {
+                    return (
+                        <div className="container">
+                            <h1>Login</h1>
+                            <div className="col-md-6">
+                                <FormField formik={formik} name="username" schema={loginSchema}/>
+                                <FormField formik={formik} name="password" schema={loginSchema}/>
+                                <div className="form-group form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                                    <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+                                </div>
+                                <div className="form-group mt-2">
+                                    <button type="submit" className="btn btn-primary" onClick={formik.submitForm}>Log in</button>
+                                    <button type="button" className="btn btn-secondary mx-2"
+                                            onClick={handleRegistration}>Don't
+                                        have an account? Register
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            }
+        </Formik>
     );
 }
 
